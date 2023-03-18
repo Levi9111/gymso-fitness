04a219df4037607d57a0b531c2d35c072e08ae46
levi9111 <captainlevi9111@gmail.com>
2023-03-18 13:36:31 +0600

    membership section added and made responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
-
 const navbar = document.querySelector(".navbar");
+const ul = document.querySelector(".ul--responsive");
 
 btn1.addEventListener("click", function () {
   btn1.style.display = "none";
   btn2.style.display = "block";
   navbar.classList.add("height");
+  ul.style.display = "flex";
 });
 
 btn2.addEventListener("click", function () {
   btn2.style.display = "none";
   btn1.style.display = "block";
   navbar.classList.remove("height");
+  ul.style.display = "none";
 });
